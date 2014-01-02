021d6257e7faf60e9763e22a66604ad0b6792acc
Debug- <franke.morello@list.ru>
2014-01-02 14:06:51 +0600

    Update CObject.h

--- Source/GameMode/GameMode/CObject.h
+++ Source/GameMode/GameMode/CObject.h
@@ -1,17 +1,18 @@
 /*
-	Copyright (c) 2014, Οελόμενό. All rights reserved.
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; version 2 of the License.
+Copyright (C) 2014  ΠΠµΠ»ΡΠΌΠµΠ½Ρ.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "Config.h"
@@ -20,4 +21,4 @@ class CObject {
 public:
 	void Create (int modelid, float x, float y, float z, float rx, float ry, float rz, float drawDistance);
 	void Destroy (int objectid);
-}; extern CObject *Object;
+}; extern CObject Object;
